466008d5d6da08c2c3ef5a87646243cdc2ebf3a1
Akhil G <akhilgalla41@gmail.com>
2023-04-03 00:39:18 +0530

    Fixed: headers and token errors

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-// const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOTI0ZjM4NjRhODM2YTJiZTFiYWZlMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MTcxODMyOCwiZXhwIjoxNjQxOTc3NTI4fQ.BHKvrS09UpdDaToxnbbJzQeat-vcSXC2LJcxD0aYo1k"
-const TOKEN = localStorage.getItem("accessToken")
+const TOKEN = localStorage.getItem("userToken")
+// console.log(TOKEN)
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  headers: { Authorization: `Bearer ${TOKEN}` },
 });
