cc6fd58a7d6db7bd1bf9848fed6679d208415437
Akhil G <akhilgalla41@gmail.com>
2023-04-25 00:24:16 +0530

    Refactor: delete unused api/index.js

--- api/App.js
+++ api/App.js
@@ -1,51 +1,124 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const mongoose = require('mongoose');
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/user");
-const multer = require("multer");
-const path = require("path");
-const swaggerUi = require('swagger-ui-express')
-const YAML = require('yamljs'); 
-const swaggerDocument = YAML.load('./swagger/swagger.yaml');
-
-
 dotenv.config();
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const contactRoute = require("./routes/contact");
+const adminRoute = require("./routes/admin");
+const multerRoute = require("./middlewares/multer");
 
+const Product = require("./models/Product");
+
+// //Morgan Middleware
+// const cors = require("cors");
+// const path = require("path");
+// const morgan = require("morgan");
+// const rfs = require("rotating-file-stream");
+
+//Morgan Middleware
+const cors = require("cors");
+const morgan = require("morgan");
+const { accessLogStream } = require("./middlewares/morgan");
+
+//Swagger API
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+//Cloudinary Images Upload through Multer Middleware
+const upload = require("./middlewares/multer")
+const fs = require('fs');
+const path = require('path');
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(async () => {
+    console.log("DB Connection Successfull!")
+
+    const count = await Product.countDocuments();
+    if (count === 0) {
+      const filePath = path.join(__dirname, '../products.json');
+      const jsonData = fs.readFileSync(filePath, { encoding: 'utf8' });
+      const products = JSON.parse(jsonData);
+
+      Product.insertMany(products)
+        .catch((err) => console.log(err))
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// let accessLogStream = rfs.createStream("access.log", {
+//   interval: "1h",
+//   path: "./access_logs"
+// });
+
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "Customer API",
+      description: "Customer API Information",
+      contact: {
+        name: "GrainHub"
+      },
+      servers: ["http://localhost:5000"]
+    }
+  },
+  // ['.routes/*.js']
+  apis: ['.routes/*.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+app.use(cors());
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname,"/images")))
-app.use(cors())
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+app.use("/api/contact", contactRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/images", multerRoute);
 
-//Add images to local storage
-const storage = multer.diskStorage({
-    destination:(req,file,cb) =>{
-        cb(null,"images")
-    },filename:(req,file,cb) =>{
-        cb(null,req.body.name);
-    },
+// app.use('/upload-images', upload.array('image'), async (req, res) => {
+//   if (req.method === 'POST') {
+//     const urls = []
+//     const files = req.files
+//     for (const file of files) {
+//       const { path } = file
+//       const newPath = await uploader(path)
+//       urls.push(newPath)
+//       fs.unlinkSync(path)
+//     }
+//     res.status(200).json({
+//       message: 'Images Uploaded Successfully',
+//       data: urls
+//     })
+//   }
+//   else {
+//     res.status(405).json({
+//       err: "Images not uploaded Successfully"
+//     })
+//   }
+// });
+
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
 });
 
-const upload = multer({storage:storage});
-app.post("/api/upload", upload.single("file"),(req,res)=>{
-    res.status(200).json("file has been uploaded");
-})
-
-
-//Routes
-
-app.use("/api/auth",authRoute);
-app.use("/api/users",userRoute);
-
-
-
-//Local host port
-app.listen("5000",()=>{
-    console.log("Backend is running ...");
-});
-
-app.use(
-    "/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument)
-)
